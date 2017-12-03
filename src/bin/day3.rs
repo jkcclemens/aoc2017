3763ccbe92b644b79ef2683d991c5a19d6df1637
Kyle Clemens <jkc.clemens@gmail.com>
2017-12-03 18:37:54 -0500

    feat: add day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,9 +1,16 @@
-const INPUT: i32 = 361_527;
+use std::collections::HashMap;
+
+const INPUT: usize = 361_527;
 
 fn main() {
-  let x = x(INPUT);
-  let y = y(INPUT);
-  println!("{}", x.abs() + y.abs());
+  println!("{}", p1());
+  println!("{}", p2());
+}
+
+fn p1() -> i32 {
+  let x = x(INPUT as i32);
+  let y = y(INPUT as i32);
+  x.abs() + y.abs()
 }
 
 fn x(n: i32) -> i32 {
@@ -18,4 +25,71 @@ fn y(n: i32) -> i32 {
     let k = (f64::from(4 * (n - 2) + 1).sqrt().floor() as i32) % 4;
     acc + (f64::from(k) * std::f64::consts::PI / 2.0).cos() as i32
   })
+}
+
+fn p2() -> usize {
+  let mut values: HashMap<(i64, i64), usize> = HashMap::new();
+  values.insert((0, 0), 1);
+
+  for (x, y) in Spiral::default() {
+    let new_value = adjacent(x, y).into_iter()
+      .filter(|pair| values.contains_key(&pair))
+      .map(|pair| values[&pair])
+      .sum();
+    if new_value > INPUT {
+      return new_value;
+    }
+    values.insert((x, y), new_value);
+  }
+  unreachable!()
+}
+
+fn adjacent(x: i64, y: i64) -> Vec<(i64, i64)> {
+  vec![
+    (x + 1, y),
+    (x + 1, y + 1),
+    (x, y + 1),
+    (x - 1, y + 1),
+    (x - 1, y),
+    (x - 1, y - 1),
+    (x, y - 1),
+    (x + 1, y - 1),
+  ]
+}
+
+struct Spiral {
+  x: i64,
+  y: i64,
+  d: i64,
+  m: i64
+}
+
+impl Default for Spiral {
+  fn default() -> Self {
+    Spiral {
+      x: 0,
+      y: 0,
+      d: 1,
+      m: 1
+    }
+  }
+}
+
+impl Iterator for Spiral {
+  type Item = (i64, i64);
+
+  fn next(&mut self) -> Option<Self::Item> {
+    loop {
+      if 2 * self.x * self.d < self.m {
+        self.x += self.d;
+        return Some((self.x, self.y));
+      }
+      if 2 * self.y * self.d < self.m {
+        self.y += self.d;
+        return Some((self.x, self.y));
+      }
+      self.d = -self.d;
+      self.m += 1;
+    }
+  }
 }
